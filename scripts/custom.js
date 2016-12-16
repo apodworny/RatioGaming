938e4e7c882d13be285e6c64f7dede28cdfc9c30
Alex Podworny <alex.podworny@hotmail.com>
2016-12-16 14:58:14 -0600

    Added side image with a script to change them randomly on page load

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,3 +1,5 @@
 $( document ).ready(function() {
-
+  var randomNo = Math.floor((Math.random() * 3) + 1);
+  var randomBg = "url('/images/bg" + randomNo + ".jpg')";
+  $('.left-image-wrapper').css("background-image", randomBg);
 });
