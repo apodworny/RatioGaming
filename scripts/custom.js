711e8ec0eaf97c5feee059a1635d17df61ef9b8b
Alex Podworny <alex.podworny@hotmail.com>
2016-12-28 16:04:25 -0600

    Added schedule page, added mobile navigation, added 3 new hero images

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
-  var randomNo = Math.floor((Math.random() * 3) + 1);
+
+  var randomNo = Math.floor((Math.random() * 6) + 1);
   var randomBg = "url('/images/bg" + randomNo + ".jpg')";
   $('.left-image-wrapper').css("background-image", randomBg);
+
+  $('.hamburger').on('click', function() {
+    $('header nav.nav-dropdown').toggleClass("hidden");
+    $('.hamburger').toggleClass("hidden");
+    $('.hamburger-close').toggleClass("hidden");
+  });
+  $('.hamburger-close').on('click', function() {
+    $('header nav.nav-dropdown').toggleClass("hidden");
+    $('.hamburger').toggleClass("hidden");
+    $('.hamburger-close').toggleClass("hidden");
+  });
 });
